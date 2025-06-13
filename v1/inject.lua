93530afa1a680a659a51e7f354e13b39fc449ade
jorge <jorgitoadame17@gmail.com>
2025-06-12 19:51:23 -0700

    new button

--- v1/inject.lua
+++ v1/inject.lua
@@ -9,6 +9,7 @@ local UI = Instance.new("Folder")
 local OpenButton = Instance.new("TextButton")
 local UICorner = Instance.new("UICorner")
 local Main = Instance.new("Frame")
+local RemoveDoors = Instance.new("TextButton")
 
 --Properties:
 
@@ -43,16 +44,28 @@ Main.BorderColor3 = Color3.fromRGB(0, 0, 0)
 Main.BorderSizePixel = 0
 Main.Position = UDim2.new(0.772169173, 0, 0.191326529, 0)
 Main.Size = UDim2.new(0, 160, 0, 173)
-Main.Visible = false
+
+RemoveDoors.Name = "RemoveDoors"
+RemoveDoors.Parent = Main
+RemoveDoors.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+RemoveDoors.BorderColor3 = Color3.fromRGB(0, 0, 0)
+RemoveDoors.BorderSizePixel = 0
+RemoveDoors.Position = UDim2.new(0.0562499985, 0, 0.0514506064, 0)
+RemoveDoors.Size = UDim2.new(0.456250012, 0, 0.0982658938, 0)
+RemoveDoors.Font = Enum.Font.SourceSans
+RemoveDoors.Text = "Remove Doors"
+RemoveDoors.TextColor3 = Color3.fromRGB(0, 0, 0)
+RemoveDoors.TextSize = 14.000
 
 -- Scripts:
 
-local function DOZF_fake_script() -- Scripts.LocalScript 
+local function HEBFWJ_fake_script() -- Scripts.LocalScript 
 	local script = Instance.new('LocalScript', Scripts)
 
 	local ScreenGui = script.Parent.Parent
 	local MainFrame = ScreenGui.UI.Main
 	local OpenButton = ScreenGui.UI.OpenButton
+	local DoorsButton = MainFrame.RemoveDoors
 	
 	OpenButton.MouseButton1Click:Connect(function() 
 		if not MainFrame.Visible then
@@ -61,5 +74,17 @@ local function DOZF_fake_script() -- Scripts.LocalScript
 			MainFrame.Visible = false
 		end	
 	end)
+	
+	DoorsButton.MouseButton1Click:Connect(function()
+		local folder = workspace:WaitForChild("Doors")
+		
+		for _, item in ipairs(folder:GetChildren()) do
+			if item.IsA("Model") then
+				item:Destroy()
+			end
+		end
+	end)
+	
+	
 end
-coroutine.wrap(DOZF_fake_script)()
+coroutine.wrap(HEBFWJ_fake_script)()
